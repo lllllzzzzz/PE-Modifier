a20e22376fef39bf01bed9754b6758326d90b951
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-24 02:02:04 -0800

    Replaced magic numbers in return statements

--- main.c
+++ main.c
@@ -1,44 +1,45 @@
 #include <windows.h>
 #include <stdio.h>
+#include <assert.h>
 
 #define NUM_GOOD_ARGS 2
 
 int main(int argc, char *argv[])
 {
     if (argc != NUM_GOOD_ARGS) {
-        fprintf(stderr, "usage: %s [filename]\n", argv[0]);
-        return -1;
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    HANDLE                  file;
-    HANDLE                  fileMap;
-    PIMAGE_DOS_HEADER       dosHeader;
-    PIMAGE_NT_HEADERS       ntHeaders;
-    PIMAGE_SECTION_HEADER   section;
-    PIMAGE_SECTION_HEADER   sectionHeader;
-    HMODULE                 user32;
-    LPBYTE                  map;
-    DWORD                   fileSize;
+    HANDLE                file;
+    HANDLE                fileMap;
+    PIMAGE_DOS_HEADER     dosHeader;
+    PIMAGE_NT_HEADERS     ntHeaders;
+    PIMAGE_SECTION_HEADER section;
+    PIMAGE_SECTION_HEADER sectionHeader;
+    HMODULE               user32;
+    LPBYTE                map;
+    DWORD                 fileSize;
 
     file = CreateFile(argv[1], GENERIC_WRITE | GENERIC_READ, FILE_SHARE_READ |
         FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (file == INVALID_HANDLE_VALUE) {
         fprintf(stderr, "Error opening %s!\n", argv[1]);
-        return -1;
+        return EXIT_FAILURE;
     }
 
     fileSize = GetFileSize(file, 0);
     if (!fileSize) {
         fprintf(stderr, "File size is 0 bytes!\n");
         CloseHandle(file);
-        return -1;
+        return EXIT_FAILURE;
     }
 
     fileMap = CreateFileMapping(file, NULL, PAGE_READWRITE, 0, fileSize, NULL);
     if (!fileMap) {
         fprintf(stderr, "Error mapping file to memory!\n");
         CloseHandle(file);
-        return -1;
+        return EXIT_FAILURE;
     }
 
     map = MapViewOfFile(fileMap, FILE_MAP_ALL_ACCESS, 0, 0, fileSize);
@@ -46,7 +47,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Error creating map view of file!\n");
         CloseHandle(fileMap);
         CloseHandle(file);
-        return -1;
+        return EXIT_FAILURE;
     }
 
     dosHeader = (PIMAGE_DOS_HEADER) map;
@@ -56,18 +57,18 @@ int main(int argc, char *argv[])
         CloseHandle(map);
         CloseHandle(fileMap);
         CloseHandle(file);
-        return -1;
+        return EXIT_FAILURE;
     }
 
     ntHeaders = (PIMAGE_NT_HEADERS) ((DWORD) map + dosHeader->e_lfanew);
     if (ntHeaders->Signature != IMAGE_NT_SIGNATURE) {
-        fprintf(stderr, "NT signature not found! This file is not a valid PE.\n");
+        fprintf(stderr, "NT signature not found! File is not a valid PE.\n");
         free(ntHeaders);
         free(dosHeader);
         CloseHandle(map);
         CloseHandle(fileMap);
         CloseHandle(file);
-        return -1;
+        return EXIT_FAILURE;
     }
 
     printf("MZ header: %X\n", dosHeader->e_magic);
@@ -79,5 +80,5 @@ int main(int argc, char *argv[])
     CloseHandle(fileMap);
     CloseHandle(file);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
